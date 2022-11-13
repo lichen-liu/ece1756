252cfae3962cdfa1bb2ec839c30079d295069cd8
lichen-liu <issac.liu@mail.utoronto.ca>
2022-11-13 02:42:41 -0500

    Add all test_from_str test cases

--- lab3/ram_mapper/test_logical_ram.py
+++ lab3/ram_mapper/test_logical_ram.py
@@ -1,9 +1,18 @@
 import unittest
-from . import logical_ram
+from .logical_ram import LogicalRam, RamMode
 
 
 class LogicalRamTestCase(unittest.TestCase):
     def test_from_str(self):
         self.assertEqual(
-            logical_ram.LogicalRam.from_str('0	0	SimpleDualPort	45	12'),
-            logical_ram.LogicalRam(circuit_id=0, ram_id=0, mode=logical_ram.RamMode.SimpleDualPort, depth=45, width=12))
+            LogicalRam.from_str('0	0	SimpleDualPort	45	12'),
+            LogicalRam(circuit_id=0, ram_id=0, mode=RamMode.SimpleDualPort, depth=45, width=12))
+        self.assertEqual(
+            LogicalRam.from_str('0	33	ROM           	256	8'),
+            LogicalRam(circuit_id=0, ram_id=33, mode=RamMode.ROM, depth=256, width=8))
+        self.assertEqual(
+            LogicalRam.from_str('5	99	TrueDualPort  	512	39'),
+            LogicalRam(circuit_id=5, ram_id=99, mode=RamMode.TrueDualPort, depth=512, width=39))
+        self.assertEqual(
+            LogicalRam.from_str('6	20	SinglePort    	2048	32'),
+            LogicalRam(circuit_id=6, ram_id=20, mode=RamMode.SinglePort, depth=2048, width=32))
