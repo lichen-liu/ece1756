82a6a1c9f0a7c1f9d44fc6d5903773091d8ce6f8
lichen-liu <issac.liu@mail.utoronto.ca>
2022-11-15 03:44:17 -0500

    all_pow2_below

--- lab3/ram_mapper/test_utils.py
+++ lab3/ram_mapper/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from ram_mapper.utils import highest_power_of_2_below
+from ram_mapper.utils import highest_pow2_below, all_pow2_below
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -8,7 +8,7 @@ class UtilsTestCase(unittest.TestCase):
         # init_logger()
         return super().setUp()
 
-    def test_highest_power_of_2_below(self):
+    def test_highest_pow2_below(self):
         result_pair = [(1, 1), (2, 2), (3, 2), (4, 4),
                        (5, 4), (6, 4), (7, 4), (8, 8),
                        (9, 8), (10, 8), (11, 8), (12, 8),
@@ -19,4 +19,11 @@ class UtilsTestCase(unittest.TestCase):
                        (257, 256), (511, 256), (512, 512), (513, 512),
                        (1023, 512), (1024, 1024), (2047, 1024), (2048, 2048)]
         for x, golden_ans in result_pair:
-            self.assertEqual(highest_power_of_2_below(x), golden_ans)
+            self.assertEqual(highest_pow2_below(x), golden_ans)
+
+    def test_all_pow2_below(self):
+        self.assertEqual(all_pow2_below(1), [1])
+        self.assertEqual(all_pow2_below(2), [2, 1])
+        self.assertEqual(all_pow2_below(15), [8, 4, 2, 1])
+        self.assertEqual(all_pow2_below(16), [16, 8, 4, 2, 1])
+        self.assertEqual(all_pow2_below(17), [16, 8, 4, 2, 1])
--- lab3/ram_mapper/utils.py
+++ lab3/ram_mapper/utils.py
@@ -1,5 +1,6 @@
 import logging
 import math
+from typing import List
 
 
 def init_logger(level=logging.DEBUG):
@@ -9,6 +10,17 @@ def init_logger(level=logging.DEBUG):
                         datefmt='%Y%m%d:%H:%M:%S', level=level)
 
 
-def highest_power_of_2_below(n: int) -> int:
+def highest_pow2_below(n: int) -> int:
     p = int(math.log(n, 2))
     return int(pow(2, p))
+
+
+def all_pow2_below(x: int) -> List[int]:
+    def all_pow2_below_helper(a: int, l: List[int]) -> List[int]:
+        if a >= 1:
+            a = highest_pow2_below(a)
+            l.append(a)
+            if a > 1:
+                return all_pow2_below_helper(a - 1, l)
+        return l
+    return all_pow2_below_helper(x, [])
