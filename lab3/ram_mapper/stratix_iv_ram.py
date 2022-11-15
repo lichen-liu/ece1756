3b51ee2b9546533f3ad2644bc94cf892ef565c21
lichen-liu <issac.liu@mail.utoronto.ca>
2022-11-15 03:44:25 -0500

    LUTRamArch

--- lab3/ram_mapper/stratix_iv_ram.py
+++ lab3/ram_mapper/stratix_iv_ram.py
@@ -10,4 +10,15 @@ class BlockRamArch(RamArch):
         return RamType.BLOCK_RAM
 
     def get_supported_ram_mode(self) -> RamMode:
-        pass
+        return RamMode.ROM or RamMode.SinglePort or RamMode.SimpleDualPort or RamMode.TrueDualPort
+
+
+class LUTRamArch(RamArch):
+    def __init__(self, id: int):
+        super().__init__(id)
+
+    def get_ram_type(self) -> RamType:
+        return RamType.LUTRAM
+
+    def get_supported_ram_mode(self) -> RamMode:
+        return RamMode.ROM or RamMode.SinglePort or RamMode.SimpleDualPort
