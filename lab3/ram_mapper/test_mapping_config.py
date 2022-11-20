3ba59edacfec2716ee9538705736bfd3c03a0e91
lichen-liu <issac.liu@mail.utoronto.ca>
2022-11-20 06:21:20 -0500

    generate_3level_CircuitRamConfig utst passed

--- lab3/ram_mapper/test_mapping_config.py
+++ lab3/ram_mapper/test_mapping_config.py
@@ -60,3 +60,41 @@ class MappingConfigTestCase(unittest.TestCase):
     LW 30 LD 1024 ID 0 S 1 P 4 Type 2 Mode SinglePort W 8 D 1024
     LW 30 LD 1 ID 1 S 1 P 2 Type 1 Mode SinglePort W 20 D 32'''
         self.assertEqual(crc.print(0), crc_expected_str)
+
+    def generate_3level_CircuitRamConfig(self):
+        prc0 = PhysicalRamConfig(id=0, num_series=1, num_parallel=4, ram_arch_id=1,
+                                 ram_mode=RamMode.SinglePort, width=20, depth=32)
+        lrc0 = LogicalRamConfig(logical_width=30, logical_depth=8, prc=prc0)
+
+        prc1 = PhysicalRamConfig(id=1, num_series=1, num_parallel=1, ram_arch_id=3,
+                                 ram_mode=RamMode.SinglePort, width=16, depth=8192)
+        lrc1 = LogicalRamConfig(logical_width=16, logical_depth=8192, prc=prc1)
+        prc2 = PhysicalRamConfig(id=2, num_series=1, num_parallel=14, ram_arch_id=2,
+                                 ram_mode=RamMode.SinglePort, width=1, depth=8192)
+        lrc2 = LogicalRamConfig(logical_width=14, logical_depth=8192, prc=prc2)
+        clrc12 = CombinedLogicalRamConfig(
+            split=RamSplitDimension.parallel, lrc_l=lrc1, lrc_r=lrc2)
+        lrc12 = LogicalRamConfig(
+            logical_width=30, logical_depth=8192, clrc=clrc12)
+
+        clrc012 = CombinedLogicalRamConfig(
+            split=RamSplitDimension.series, lrc_l=lrc0, lrc_r=lrc12)
+        lrc012 = LogicalRamConfig(
+            logical_width=30, logical_depth=8200, clrc=clrc012)
+
+        crc = CircuitRamConfig(circuit_id=3, ram_id=8,
+                               num_extra_lut=32, lrc=lrc012)
+        return crc
+
+    def test_CircuitRamConfig_3level_verify(self):
+        crc = self.generate_3level_CircuitRamConfig()
+        self.assertTrue(crc.verify())
+
+    def test_CircuitRamConfig_3level_print(self):
+        crc = self.generate_3level_CircuitRamConfig()
+        crc_expected_str = '''3 8 32 LW 30 LD 8200 series
+    LW 30 LD 8 ID 0 S 1 P 4 Type 1 Mode SinglePort W 20 D 32
+    LW 30 LD 8192 parallel
+        LW 16 LD 8192 ID 1 S 1 P 1 Type 3 Mode SinglePort W 16 D 8192
+        LW 14 LD 8192 ID 2 S 1 P 14 Type 2 Mode SinglePort W 1 D 8192'''
+        self.assertEqual(crc.print(0), crc_expected_str)
