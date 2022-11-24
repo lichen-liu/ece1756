e343b221356306461ede2d1b011f70125d6bf9c2
lichen-liu <issac.liu@mail.utoronto.ca>
2022-11-24 02:10:30 -0500

    Print geomean area

--- lab3/ram_mapper/driver.py
+++ lab3/ram_mapper/driver.py
@@ -1,7 +1,8 @@
 import logging
+import statistics
+from typing import List
 
 from .utils import init_logger, sorted_dict_items
-
 from . import utils
 from . import siv_heuristics
 from . import transform
@@ -48,8 +49,16 @@ def verbosity_to_logging_level(verbose_count: int) -> int:
         return logging.DEBUG
 
 
-# python3 -m ram_mapper --lb=test0/logic_block_count.txt --lr=test0/logical_rams.txt --out=test0/mapping.txt
+def geomean_fpga_area(fpga_area_list: List[int]) -> float:
+    factor = 10000000.0
+    fpga_area_list_scaled_down = map(
+        lambda area: float(area)/factor, fpga_area_list)
+    geomean_scaled_down = statistics.geometric_mean(fpga_area_list_scaled_down)
+    geomean = geomean_scaled_down * factor
+    return geomean
 
+
+# python3 -m ram_mapper --lb=test0/logic_block_count.txt --lr=test0/logical_rams.txt --out=test0/mapping.txt
 
 def run(args):
     # Logger setting for module execution mode
@@ -77,11 +86,19 @@ def run(args):
         lcs), 'Final mapping result must contain same number of circuits as logical_ram input'
     acc.serialize_to_file(mapping_filename)
 
+    # Calculate FPGA area
     if not args.no_area_report:
         logging.warning('=================')
         logging.warning('Final Area Report')
-        # Print area
-        for circuit_id, cc in sorted_dict_items(acc.circuits):
-            siv_heuristics.calculate_fpga_area_for_circuit(
-                ram_archs=ram_archs, logical_circuit=lcs[circuit_id], circuit_config=cc, verbose=True)
+    circuit_fpga_area_list = list()
+    for circuit_id, cc in sorted_dict_items(acc.circuits):
+        circuit_fpga_area = siv_heuristics.calculate_fpga_area_for_circuit(
+            ram_archs=ram_archs, logical_circuit=lcs[circuit_id], circuit_config=cc, verbose=(not args.no_area_report))
+        circuit_fpga_area_list.append(circuit_fpga_area)
+    if not args.no_area_report:
         logging.warning('=================')
+
+    # Calculate FPGA area geomean
+    fpga_area_geomean = geomean_fpga_area(circuit_fpga_area_list)
+    logging.warning(
+        f'Geometric Average Area for {len(circuit_fpga_area_list)} circuits: {fpga_area_geomean:.6E}')
